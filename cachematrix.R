9cbb04bd48fa2e37b80e3bd7d2c81ec81995a103
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2015-05-24 20:42:10 +0200

    Newer version of cachematrix.R - added some comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Funtion creates a special matrix, which is
+## a list containing setters and getters
+## for matrix object itself and/or it's inverse
 makeCacheMatrix <- function(x = matrix()) {
    s <- NULL
    set <- function(y) {
       s <<- y
       s <<- NULL
+      # operator << cause a search to made through parent
+      # environments for an existing definition of the variable
+      # being assigned. If such a variable is found (and its binding
+      # is not locked) then its value is redefined,
+      # otherwise assignment takes place in the global environment.
    }
    get <- function() x
    setsolve <- function(solve) s <<- solve
@@ -15,22 +18,27 @@ makeCacheMatrix <- function(x = matrix()) {
    list(set = set, get = get,
         setsolve = setsolve,
         getsolve = getsolve)
+   # return vector-type list of functions - setters and getters
 }
 
 
-## Write a short comment describing this function
-
+## Below function solves the matrix created with function
+## makeCacheMatrix. It first checks if the inverse
+## has been already calculated, if yes, it returns the cached result,
+## otherwise it calls solve() from the base package and then
+## retuns the solution, caching it as well.
 cacheSolve <- function(x, ...) {
    ## Return a matrix that is the inverse of 'x'
    s <- x$getsolve()
-   if(!is.null(s)) {
+   if(!is.null(s)) { # if the solution was cached, return it without
+                     # executing the calculations
       message("getting cached data")
       return(s)
    }
-   data <- x$get()
-   s <- solve(data, ...)
-   x$setsolve(s)
-   s
+   data <- x$get()  # otherwise, first put the matrix in 'data'
+   s <- solve(data, ...)  # and solve the matrix with 'data' as arg
+   x$setsolve(s)  # cache the solution
+   s  # return the solution
 }
 
 # Tests for correctness
@@ -62,3 +70,16 @@ stopifnot(
          matrix(c(1, 1, 0, 1), 2, 2))),
       matrix(c(1, -1, 0, 1), 2, 2) )
 )
+
+# Interactive check
+
+m <- matrix(c(-1, -2, 1, 1), 2,2)
+x <- makeCacheMatrix(m)
+x$get()
+
+inv <- cacheSolve(x)
+inv
+
+inv <- cacheSolve(x)
+# should print 'getting cached data'
+inv
